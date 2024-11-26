804f8ba5920fddfb7dd283517e05e911c3c34cde
Ted Yun <tedyun@users.noreply.github.com>
2024-11-25 23:32:45 -0500

    Update email_sender.py to support multiple TO addresses

--- email_sender.py
+++ email_sender.py
@@ -2,7 +2,7 @@ import argparse
 import smtplib
 from email.headerregistry import Address
 from email.message import EmailMessage
-from typing import Any, Union, Sequence
+from typing import Any, Sequence
 
 
 USERNAME_FILE_NAME = 'username'
@@ -21,24 +21,25 @@ def read_from_file(fpath: str) -> str:
 
 def parse_args() -> argparse.Namespace:
   parser = argparse.ArgumentParser(description='Send args.')
-  parser.add_argument('--to_addr', help='The TO address.',
-                      default=read_from_file(TEST_EMAIL_FILE_NAME))
+  parser.add_argument('--to_addrs', help='The comma-separated list of TO addresses.',
+                      default=read_from_file(TEST_EMAIL_FILE_NAME), type=str)
   parser.add_argument('--subject', help='The subject.', default=DEFAULT_SUBJECT)
   parser.add_argument('--content', help='The content.', default=DEFAULT_CONTENT)
   parser.add_argument('--from_name', help='The content.', default=DEFAULT_FROM_NAME)
   args = parser.parse_args()
+  if args.to_addrs:
+    args.to_addrs = [s.strip() for s in args.to_addrs.split(",")]
+  else:
+    raise ValueError('Must specify --to_addrs.')
   print('Input args:')
   for arg in vars(args):
-      print('--{}: {}'.format(arg, getattr(args, arg)))
+    print('--{}: {}'.format(arg, getattr(args, arg)))
   return args
 
 
-def send_email(to_addrs: Union[str, Sequence[str]], subject: str, content: str,
+def send_email(to_addrs: Sequence[str], subject: str, content: str,
                smtp_url: str = GMAIL_SMTP_URL, smtp_port: int = GMAIL_SMTP_PORT,
                from_name: str = '') -> None:
-  if isinstance(to_addrs, str):
-    to_addrs = [to_addrs]
-
   from_email = read_from_file(USERNAME_FILE_NAME)
   pwd = read_from_file(PWD_FILE_NAME)
 
@@ -61,7 +62,7 @@ def send_email(to_addrs: Union[str, Sequence[str]], subject: str, content: str,
 def main():
   args = parse_args()
   send_email(
-    to_addrs=args.to_addr,
+    to_addrs=args.to_addrs,
     subject=args.subject,
     content=args.content,
     from_name=args.from_name,
